8e036f113239bbd36eb36b89a34ec93124b0f835
Noam <Noam2710@gmail.com>
2019-11-30 13:49:24 +0200

    Illustration of the best solution

--- Assignement2.py
+++ Assignement2.py
@@ -1,5 +1,4 @@
 from termcolor import colored
-import copy
 import random
 import numpy
 from deap import base, creator, tools, algorithms
@@ -28,11 +27,14 @@ class Maze:
         self.best_way_length = 9
 
     def print_maze(self):
+        temp = self.maze[self.current_loc[0]][self.current_loc[1]]
+        self.maze[self.current_loc[0]][self.current_loc[1]] = 'C'
         for line in self.maze:
             line_to_print = ''
             for column in line:
                 line_to_print += Maze.get_element_by_column(column) + ' '
             print(line_to_print)
+        self.maze[self.current_loc[0]][self.current_loc[1]] = temp
 
     @classmethod
     def get_element_by_column(cls, column):
@@ -40,45 +42,10 @@ class Maze:
             return colored(u"\u2588",'red')
         elif column == ' ':
             return colored(u"\u2588",'white')
+        elif column == 'C':
+            return colored(u"\u2588",'magenta')
         else:
             return colored(u"\u2588",'green')
-
-    def get_length_to_reach_the_goal(self, genes):
-        self.current_loc = self.start_loc
-        for idx, step in enumerate(genes):
-            if step == 'RIGHT':
-                new_index = [self.current_loc[0],self.current_loc[1]+1]
-                valid_step = self.check_if_valid_step(new_index)
-            if step == 'LEFT':
-                new_index = [self.current_loc[0], self.current_loc[1] - 1]
-                valid_step = self.check_if_valid_step(new_index)
-            if step == 'UP':
-                new_index = [self.current_loc[0]-1, self.current_loc[1]]
-                valid_step = self.check_if_valid_step(new_index)
-            if step == 'DOWN':
-                new_index = [self.current_loc[0] + 1, self.current_loc[1]]
-                valid_step = self.check_if_valid_step(new_index)
-
-            if valid_step:
-                self.current_loc = new_index
-
-            if self.current_loc == self.end_loc:
-                break
-
-        return idx+1
-
-    def check_if_valid_step(self,new_index):
-        number_of_rows = len(self.maze)
-        number_of_columns = len(self.maze[0])
-
-        if new_index[0] < 0 or new_index[0] > number_of_rows-1:
-            return False
-        if new_index[1] < 0 or new_index[1] > number_of_columns-1:
-            return False
-        if self.maze[new_index[0]][new_index[1]] == WALL:
-            return False
-
-        return True
 
 
 def get_length_to_reach_the_goal(individual):
@@ -157,6 +124,46 @@ def register_stats():
 
 
 test_maze = Maze()
+
+
+def print_initial_maze():
+    print('--------------------')
+    test_maze.print_maze()
+    print('--------------------')
+
+
+def print_the_best_solution():
+    maze = Maze()
+    best_path = best_ind.items
+    print("Step No. 0")
+    maze.print_maze()
+    for idx, step in enumerate(best_path[0]):
+        step = GENES_OPTIONS[step]
+        if step == 'RIGHT':
+            new_index = [maze.current_loc[0], maze.current_loc[1] + 1]
+            valid_step = check_if_valid_step(new_index, maze.maze)
+        if step == 'LEFT':
+            new_index = [maze.current_loc[0], maze.current_loc[1] - 1]
+            valid_step = check_if_valid_step(new_index, maze.maze)
+        if step == 'UP':
+            new_index = [maze.current_loc[0] - 1, maze.current_loc[1]]
+            valid_step = check_if_valid_step(new_index, maze.maze)
+        if step == 'DOWN':
+            new_index = [maze.current_loc[0] + 1, maze.current_loc[1]]
+            valid_step = check_if_valid_step(new_index, maze.maze)
+
+        if valid_step:
+            maze.current_loc = new_index
+
+        print("Step No. {}".format(str(idx+1)))
+        maze.print_maze()
+
+        if maze.current_loc == maze.end_loc:
+            break
+
+
+
+print_initial_maze()
 creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
 creator.create("Individual", list, fitness=creator.FitnessMin)
 
@@ -168,20 +175,23 @@ toolbox.register("individual", tools.initRepeat, creator.Individual,
 toolbox.register("population", tools.initRepeat, list, toolbox.individual)
 toolbox.register("evaluate", get_length_to_reach_the_goal)
 toolbox.register("mate", tools.cxOnePoint)
-toolbox.register("mutate", tools.mutUniformInt, low=0, up=3, indpb=0.08)
-toolbox.register("select", tools.selTournament, tournsize=5)
+toolbox.register("mutate", tools.mutUniformInt, low=0, up=3, indpb=0.5)
+toolbox.register("select", tools.selTournament, tournsize=10)
 
 
 pop = toolbox.population(n=POP_SIZE)
 fitnesses = toolbox.map(toolbox.evaluate, pop)
-Best_2_ind = tools.HallOfFame(2)
+best_ind = tools.HallOfFame(1)
 update_fitnesses_to_population()
 
 
 stats = tools.Statistics(key=lambda ind: ind.fitness.values)
 register_stats()
 
-pop, logbook = algorithms.eaSimple(pop, toolbox, cxpb=0.5, mutpb=0.2, ngen=1000, stats=stats, halloffame=Best_2_ind, verbose=True)
+pop, logbook = algorithms.eaSimple(pop, toolbox, cxpb=0.5, mutpb=0.2, ngen=500, stats=stats, halloffame=best_ind,
+                                   verbose=True)
+
+print_the_best_solution()
 print_graph(logbook)
 
 
